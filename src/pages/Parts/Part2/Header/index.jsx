1249fc199186e71212530f88fda84db9b72e41da
Zhaojiajie1 <2875854612@qq.com>
2022-11-04 17:53:54 +0800

    feat: add layout and iconfont

---
+++ src/pages/Parts/Part2/Header/index.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { Image } from "antd";
+
+import svg from "../../../../assets/back.svg";
+
+
+export default function Header() {
+
+  return (
+    <>
+      <div className="p-header">
+        <Link to="/home" className="back-home">
+          <Image height={17} src={svg} preview={false} />
+          <div className="txt">
+            This is a line of statements used for placeholder
+          </div>
+        </Link>
+      </div>
+    </>
+  )
+}
--- src/pages/Parts/Part2/Part2.jsx
+++ src/pages/Parts/Part2/Part2.jsx
@@ -1,12 +1,59 @@
 import React, { useState } from "react";
-
-
+import Header from './Header/index'
+import './Part2.scss'
 export function Part2() {
-
 
   return (
     <div className="Part2">
-      start
+      <Header />
+      <div className="toolBar">
+        <button className="singleTool" >
+          <span className="iconfont toolIcons">&#xe635;</span>
+          <div className="txt"></div>
+        </button>
+
+        <button className="singleTool" >
+          <span className="iconfont toolIcons">&#xe63a;</span>
+          <div className="txt">去噪</div>
+        </button>
+
+        <button className="singleTool" >
+          <span className="iconfont toolIcons">&#xe654;</span>
+          <div className="txt">增强</div>
+        </button>
+
+        <button className="singleTool">
+          <span className="iconfont toolIcons">&#xe605;</span>
+          <div className="txt">左右翻转</div>
+        </button>
+
+        <button className="singleTool" >
+          <span className="iconfont toolIcons">&#xe606;</span>
+          <div className="txt">上下翻转</div>
+        </button>
+
+        <button className="singleTool" >
+          <span className="iconfont toolIcons">&#xe7cd;</span>
+          <div className="txt">剪裁</div>
+        </button>
+        <button className="uploadTool" >
+          <div className="txt">上传</div>
+          <input type="file" style={{ display: "none" }} webkitdirectory="true" />
+        </button>
+      </div>
+
+      <div className="p-detail">
+        <div className="p-picList">
+          <div className="showPic">
+            {
+            }
+          </div>
+        </div>
+
+        <div className="detailPicBox">
+          <div className="detailPic" onContextMenu={() => false} onMouseDown={() => false} ></div>
+        </div>
+      </div>
     </div>
   );
 }
