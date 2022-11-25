80e51b941159997e25a63252969ff825c772a72e
jiejie1221 <3212960476@qq.com>
2022-11-25 14:11:52 +0800

    none

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
-const { createProxyMiddleware } = require("http-proxy-middleware")
-module.exports = (app) => {
-    app.use(
-        createProxyMiddleware(
-            "/api", {
-            target: "http://43.142.168.114:8001/",
-            changeOrigin: true,
-            secure: false, 
-            pathRewrite: {
-                '^/api': ''
-            }
-        })
-    )
-}
+// const { createProxyMiddleware } = require("http-proxy-middleware")
+// module.exports = (app) => {
+//     app.use(
+//         createProxyMiddleware(
+//             "/api", {
+//             target: "http://43.142.168.114:8001/",
+//             changeOrigin: true,
+//             secure: false, 
+//             pathRewrite: {
+//                 '^/api': ''
+//             }
+//         })
+//     )
+// }
 
