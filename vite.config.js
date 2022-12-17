ca438e12588b56398981b01015f8213eba69d6c8
haloerkay <1422369232@qq.com>
2022-12-17 23:14:53 +0800

    feat:  upgrade interface

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/file': {
-        target: 'http://43.142.168.114:8001', 
-        changeOrigin: true, //支持跨域
-      }
+      
     }
   }
 })
